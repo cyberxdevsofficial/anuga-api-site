a5ac341e045476a7f50c00e1f7c6f5737321c88e
cyberxdevsofficial <cyberxdevsofficial@gmail.com>
2025-09-23 18:30:08 +0530

    Create config.js

---
+++ backend/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  GITHUB_CLIENT_ID: "Ov23liCHdgCSJ4a5yC7a",
+  GITHUB_CLIENT_SECRET: "47ef0f17b3cfad8ef6caeed995c18fd8ae576a8f",
+  SESSION_SECRET: "super_secret_session_key",
+  CALLBACK_URL: "https://api-anugasenithu.vercel.app/auth/github/callback"
+};
